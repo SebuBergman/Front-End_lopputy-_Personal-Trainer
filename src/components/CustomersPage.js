61fffe2c31db8fdc7799ebfbdb66bbbc508c0ee7
SebuBergman <basti.bergman@gmail.com>
2022-05-03 10:14:48 +0300

    delete working

--- src/components/CustomersPage.js
+++ src/components/CustomersPage.js
@@ -8,26 +8,37 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function CustomersPage() {
-    const [cars, setCars] = useState([]);
+    const [customer, setCustomer] = useState([]);
     const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
-        fetchCars();
+        fetchCustomers();
     }, []);
 
-    const fetchCars = () => {
+    const fetchCustomers = () => {
         fetch("https://customerrest.herokuapp.com/api/customers")
         .then(response => response.json())
-        .then(data => setCars(data.content))
+        .then(data => setCustomer(data.content))
+        .catch(err => console.error(err))
     }
 
-    const deleteCar = (link) => {
+    const deleteCustomer = (link) => {
+        if (window.confirm('Are you sure?')){
+        fetch(link.value, {method: 'DELETE'})
+        .then(res => fetchCustomers())
+        .catch(err => console.error(err))
+       }
+   }
+
+/*
+    const deleteCustomer = (link) => {
         if (window.confirm('Are you sure?')) {
         fetch(link, { method: 'DELETE' })
             .then(response => {
             if (response.ok) {
                 setOpen(true);
-                fetchCars();
+                fetchCustomers();
             }
             else {
                 alert('Something went wrong');
@@ -35,6 +46,46 @@ function CustomersPage() {
             })
         }
     }
+
+    const addCustomer = (customer) => {
+        fetch(process.env.REACT_APP_API_URL,{
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(customer)
+            })
+            .then(response => {
+            if (response.ok) {
+                fetchCustomers();
+            }
+            else {
+                alert('Something went wrong when adding car');
+            }
+            })
+            .catch(err => console.error(err))
+            }
+
+    const deleteCustomer = (params) => {
+        console.log(params);
+    }
+
+    const updateCustomer = (updatedCustomer, link) => {
+    fetch(link, {
+      method: 'PUT',
+      headers: {'Content-type':'application/json'},
+      body: JSON.stringify(updatedCustomer)
+    })
+    .then(response => {
+      if (response.ok) {
+        setMsg('Car edited succesfully');
+        setOpen(true);
+        fetchCustomers();
+      }
+      else {
+        alert('Something went wrong!');
+      }
+    })
+    .catch(err => console.error(err))
+  }*/
 
     const columns = [
         { field: 'firstname', sortable: true, filter: true, width: 140 },
@@ -45,13 +96,10 @@ function CustomersPage() {
         { field: 'postcode', sortable: true, filter: true, width: 110 },
         { field: 'city', sortable: true, filter: true, width: 110 },
         {
-        headerName: '',
+        headerName: 'Self',
         width: 100,
-        field: '_links.self.href',
-        cellRenderer: params =>
-            <IconButton color="error" onClick={() => deleteCar(params.value)}>
-            <DeleteIcon />
-            </IconButton>
+        field: 'links.0.href',
+        cellRenderer: params => <button onClick={() => deleteCustomer(params)}>Delete</button>
         }
     ]
 
@@ -60,7 +108,7 @@ function CustomersPage() {
         <div className="ag-theme-material" style={{ height: 600, width: '90%' }}>
             <AgGridReact
             columnDefs={columns}
-            rowData={cars}
+            rowData={customer}
             pagination={true}
             paginationPageSize={10}
             suppressCellFocus={true}

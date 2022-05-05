3131fdda176c44ae063b70a5639d2ca97167939b
SebuBergman <basti.bergman@gmail.com>
2022-05-05 20:24:07 +0300

    customers & trainings & calendar page working and added. Also csv exporting. Still need to add statistics page

--- src/components/CustomersPage.js
+++ src/components/CustomersPage.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback, useState, useRef, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import Snackbar from '@mui/material/Snackbar';
+import Addcustomer from './AddCustomer';
+import EditCustomer from './EditCustomer';
+import Addtraining from './AddTraining';
 
+import '../App.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
+
 function CustomersPage() {
+    const gridRef = useRef();
     const [customer, setCustomer] = useState([]);
     const [open, setOpen] = useState(false);
     const [msg, setMsg] = useState('');
@@ -18,105 +25,139 @@ function CustomersPage() {
 
     const fetchCustomers = () => {
         fetch("https://customerrest.herokuapp.com/api/customers")
-        .then(response => response.json())
-        .then(data => setCustomer(data.content))
-        .catch(err => console.error(err))
+            .then(response => response.json())
+            .then(data => setCustomer(data.content))
+            .catch(err => console.error(err))
     }
 
-    const deleteCustomer = (link) => {
-        if (window.confirm('Are you sure?')){
-        fetch(link.value, {method: 'DELETE'})
-        .then(res => fetchCustomers())
-        .catch(err => console.error(err))
-       }
-   }
+    const onExportClick = useCallback(() => {
+        gridRef.current.api.exportDataAsCsv();
+    }, []);
 
-/*
     const deleteCustomer = (link) => {
-        if (window.confirm('Are you sure?')) {
-        fetch(link, { method: 'DELETE' })
-            .then(response => {
+        if (window.confirm("Are you sure you want to delete this customer?")) {
+        fetch(link, {method: 'DELETE'})
+        .then(response => {
             if (response.ok) {
+                setMsg("Customer deleted successfully");
                 setOpen(true);
                 fetchCustomers();
             }
             else {
-                alert('Something went wrong');
+                alert("Something went wrong!");
             }
-            })
+        })
         }
     }
 
     const addCustomer = (customer) => {
-        fetch(process.env.REACT_APP_API_URL,{
+        fetch("https://customerrest.herokuapp.com/api/customers",{
             method: 'POST',
             headers: {'Content-type':'application/json'},
             body: JSON.stringify(customer)
-            })
-            .then(response => {
+        })
+        .then(response => {
             if (response.ok) {
+                setMsg('Customer added successfully');
+                setOpen(true);
                 fetchCustomers();
             }
             else {
-                alert('Something went wrong when adding car');
+                alert('Something went wrong when adding a customer!');
+            }
+        })
+        .catch(err => console.error(err))
+    }
+
+    const addTraining = training => {
+        fetch("https://customerrest.herokuapp.com/api/trainings",{
+            method: "POST",
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(training)
+        })
+        .then(response => {
+            if (response.ok) {
+                setMsg("Training was added succesfully");
+                setOpen(true);
+                fetchCustomers();
+            }
+            else {
+                alert("Something went wrong with adding a training!")
             }
             })
             .catch(err => console.error(err))
-            }
-
-    const deleteCustomer = (params) => {
-        console.log(params);
+        
     }
 
     const updateCustomer = (updatedCustomer, link) => {
-    fetch(link, {
-      method: 'PUT',
-      headers: {'Content-type':'application/json'},
-      body: JSON.stringify(updatedCustomer)
-    })
-    .then(response => {
-      if (response.ok) {
-        setMsg('Car edited succesfully');
-        setOpen(true);
-        fetchCustomers();
-      }
-      else {
-        alert('Something went wrong!');
-      }
-    })
-    .catch(err => console.error(err))
-  }*/
+        fetch(link, {
+            method: 'PUT',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify(updatedCustomer)
+        })
+        .then(response => {
+            if (response.ok) {
+                setMsg("Customer edited succesfully");
+                setOpen(true);
+                fetchCustomers();
+            }
+            else {
+                alert("Something went wrong with updating customer details!");
+            }
+        })
+        .catch(err => console.error(err))
+    }
 
     const columns = [
         { field: 'firstname', sortable: true, filter: true, width: 140 },
         { field: 'lastname', sortable: true, filter: true, width: 140 },
-        { field: 'email', sortable: true, filter: true },
-        { field: 'phone', sortable: true, filter: true },
-        { field: 'streetaddress', sortable: true, filter: true },
-        { field: 'postcode', sortable: true, filter: true, width: 110 },
+        { field: 'email', sortable: true, filter: true,  width: 180 },
+        { field: 'phone', sortable: true, filter: true,  width: 140},
+        { field: 'streetaddress', sortable: true, filter: true, width: 150 },
+        { field: 'postcode', sortable: true, filter: true, width: 120 },
         { field: 'city', sortable: true, filter: true, width: 110 },
+        { 
+            headerName: '',
+            width: 90,
+            field: 'links.0.href',
+            cellRenderer: params => <Addtraining addTraining={addTraining} params={params} />
+        },
         {
-        headerName: 'Self',
-        width: 100,
-        field: 'links.0.href',
-        cellRenderer: params => <button onClick={() => deleteCustomer(params)}>Delete</button>
+            headerName: '',
+            width: 90,
+            field: 'links.0.href',
+            cellRenderer: params => <EditCustomer updateCustomer={updateCustomer} params={params} />
+        },
+        {
+            headerName: '',
+            width: 90,
+            field: 'links.0.href',
+            cellRenderer: params => 
+            <IconButton color="error" onClick={() => deleteCustomer(params.value)}>
+                <DeleteIcon />
+            </IconButton>
         }
     ]
 
     return (
         <>
+        <IconButton id="Exportbutton" onClick={() => onExportClick()}>
+            <FileDownloadIcon />Export
+        </IconButton>
+        <Addcustomer addCustomer={addCustomer} />
         <div className="ag-theme-material" style={{ height: 600, width: '90%' }}>
             <AgGridReact
-            columnDefs={columns}
-            rowData={customer}
-            pagination={true}
-            paginationPageSize={10}
-            suppressCellFocus={true}
+                ref={gridRef}
+                columnDefs={columns}
+                rowData={customer}
+                pagination={true}
+                paginationPageSize={10}
+                suppressCellFocus={true}
             />
         </div>
         <Snackbar
             open={open}
-            message="Car deleted"
+            message={msg}
             autoHideDuration={3000}
             onClose={() => setOpen(false)}
         />
